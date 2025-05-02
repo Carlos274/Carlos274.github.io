9c6910a5625e91309e30250c59e76444024f573b
SENKA <58215091+Carlos274@users.noreply.github.com>
2025-05-02 10:40:24 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function armazenarvalor(){
 
      //armazena o valor que o usario digitou
-     var userImput = document.getElementById("userImput").value;
+     var userInput = document.getElementById("userInput").value;
 
     //exibe o valor armazenado
     console.log("o valor da variavel é: " + userInput);
