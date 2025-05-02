eb40c59e863622bfb43e751e325dd1993bb628ba
SENKA <58215091+Carlos274@users.noreply.github.com>
2025-05-02 10:39:01 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 function armazenarvalor(){
 
-//armazena o valor que o usario digitou
-var userimput = document.gerElementById("userImput").value;
+     //armazena o valor que o usario digitou
+     var userImput = document.getElementById("userImput").value;
 
-//exibe o valor armazenado
-console.log("o valor da variavel é: " + userInput);
+    //exibe o valor armazenado
+    console.log("o valor da variavel é: " + userInput);
 
-//atualiza o conteudo
-document.getElementById("valorInserido").innerText = " O valor inserido é: " = userInput;
+    //atualiza o conteudo
+    document.getElementById("valorInserido").innerText = " O valor inserido é: " = userInput;
+
 
 }
